e42d96f4153276070f9c235712c086d37863efd8
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-09-14 09:29:31 +0700

    16.1.3 Làm giao diện danh sách sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,17 +1,10 @@
 const Product = require('../../models/product.model');
-// [GET] /product
+// [GET] /admin/product
 module.exports.index = async (req, res) => {
     try {
         const products = await Product.find();
-        
-        const newProducts = products.map (item => {
-            item.priceNew = ((item.price * (100 - item.discountPercentage)) / 100).toFixed();
-            return item;
-        })
 
-        console.log(newProducts)
-
-        res.render("client/pages/products/index.pug", {
+        res.render("admin/pages/products/index.pug", {
             pageTitle: "Danh sach san pham",
             products: products
         })
