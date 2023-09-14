58de74f8f0abefc851b858d5c4cfe8f3339de859
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-09-14 08:59:19 +0700

    16.1.2 Xây dựng layout chung

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require('mongoose');
 require("dotenv").config();
 const route = require("./routes/client/index.route");
-const routeAdmin =require('./routes/admin/index.route')
-const database = require('./config/database')
+const routeAdmin = require('./routes/admin/index.route')
+const database = require('./config/database');
+
+const systemConfig = require('./config/system')
 
 
 const app = express();
@@ -14,6 +16,10 @@ app.set("view engine", "pug");
 
 // use static file
 app.use(express.static("public"));
+
+// Variable
+app.locals.prefixAdmin = systemConfig.prefix_admin;
+//End Variable
 
 //route
 route(app);
