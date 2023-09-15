dc7881312d4c17381ef30fbbd1d2c0fd86e6316c
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-09-15 09:42:52 +0700

    17.1.2 Tối ưu phần tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require('../../models/product.model');
-const filterStatusHelper = require("../../helper/filterStatus")
+const filterStatusHelper = require("../../helper/filterStatus");
+const searchHelper = require("../../helper/search.js")
 // [GET] /admin/product
 module.exports.index = async (req, res) => {
     try {
@@ -7,17 +8,16 @@ module.exports.index = async (req, res) => {
         const filterStatus = filterStatusHelper(req.query)
         //End filter status
 
+        // Search
+        let objectSearch = searchHelper(req.query);
         let find = {
             deleted: false,
         }
         if(req.query.status){
             find.status = req.query.status;
         }
-        let keyword = ""
-        if(req.query.keyword) {
-            keyword = req.query.keyword;
-            const regex = new RegExp(keyword, "i")
-            find.title = regex;
+        if(req.query.keyword){
+            find.title = objectSearch.regex;
         }
 
         const products = await Product.find(find);
@@ -26,7 +26,7 @@ module.exports.index = async (req, res) => {
             pageTitle: "Danh sach san pham",
             products: products,
             filterStatus: filterStatus,
-            keyword: keyword
+            keyword: objectSearch.keyword
         })
 
     } catch (error) {
