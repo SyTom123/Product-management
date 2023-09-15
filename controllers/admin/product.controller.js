5be3754b68d1eee280b6bcdd7f22d5e0d3ac6fa2
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-09-15 08:01:58 +0700

    16.3 Tính năng lọc theo trạng thái

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,14 +2,40 @@ const Product = require('../../models/product.model');
 // [GET] /admin/product
 module.exports.index = async (req, res) => {
     try {
-        const products = await Product.find({
+        let filterStatus = [
+            {
+                name: "Tất cả",
+                status: "",
+                class: ""
+            },
+            {
+                name: "Hoạt động",
+                status:'active',
+                class: ""
+            },
+            {
+                name: "Dừng hoạt động",
+                status: "inactive",
+                class: ""
+            },
+        ]
+        let find = {
             deleted: false,
-           
-        });
+        }
+        if(req.query.status) {
+            const index = filterStatus.findIndex(item => item.status == req.query.status);
+            filterStatus[index].class = "active";
+        } else {
+            filterStatus[0].class = "active";
+        }
+        console.log(filterStatus);
+
+        const products = await Product.find(find);
 
         res.render("admin/pages/products/index.pug", {
             pageTitle: "Danh sach san pham",
-            products: products
+            products: products,
+            filterStatus: filterStatus
         })
     } catch (error) {
         console.log(error)
