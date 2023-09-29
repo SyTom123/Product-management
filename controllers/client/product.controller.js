b72976835ef07b1861d6293a734b534b89e05204
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-09-29 14:32:21 +0700

    Làm trang chi tiết sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,22 +1,39 @@
 const Product = require('../../models/product.model');
 // [GET] /product
 module.exports.index = async (req, res) => {
+
+    const products = await Product.find({
+        deleted: false,
+        status: "active"
+    }).sort({position: "desc"});
+    
+    const newProducts = products.map (item => {
+        item.priceNew = ((item.price * (100 - item.discountPercentage)) / 100).toFixed();
+        return item;
+    })
+
+    res.render("client/pages/products/index.pug", {
+        pageTitle: "Danh sach san pham",
+        products: products
+    })
+}
+module.exports.detail = async (req, res) => {
     try {
-        const products = await Product.find({
+        const slug = req.params.slug;
+        const product = await Product.findOne({
+            slug: slug,
             deleted: false,
             status: "active"
-        }).sort({position: "desc"});
-        
-        const newProducts = products.map (item => {
-            item.priceNew = ((item.price * (100 - item.discountPercentage)) / 100).toFixed();
-            return item;
-        })
+        });
+        if(!product) {
+            return res.redirect("/products");
+        }
+        res.render("client/pages/products/detail", {
+            pageTitle: "Chi tiết sản phẩm",
+            product: product
+        });
 
-        res.render("client/pages/products/index.pug", {
-            pageTitle: "Danh sach san pham",
-            products: products
-        })
     } catch (error) {
-        console.log(error)
+        res.redirect("/");
     }
 }
