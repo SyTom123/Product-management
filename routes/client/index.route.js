4ed90a22c715470b91234eb7f03c2491e54234da
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-10-16 08:24:44 +0700

    Thêm sản phẩm vào giỏ hàng

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,13 +1,19 @@
-const categoryMiddleware = require("../../middlewares/client/category.middleware")
+const categoryMiddleware = require("../../middlewares/client/category.middleware");
+const cartMiddleware = require("../../middlewares/client/cart.middleware");
 const productRouter = require('./product.route');
 const homeRouter = require ('./home.route');
 const searchRouter = require ('./search.route.js');
+const cartRouter = require ('./cart.route.js');
 
 module.exports = (app) => {
     app.use(categoryMiddleware.category);
+
+    app.use(cartMiddleware.cartId);
+
     app.use("/", homeRouter);
 
     app.use("/products", productRouter);
 
     app.use("/search", searchRouter);
+    app.use("/cart", cartRouter);
 }
