ce7b70cec35c07fe125e636966c787ddbf7c8886
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-10-18 10:47:31 +0700

    Làm giao diện trang đặt hàng

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,6 +4,7 @@ const productRouter = require('./product.route');
 const homeRouter = require ('./home.route');
 const searchRouter = require ('./search.route.js');
 const cartRouter = require ('./cart.route.js');
+const checkoutRouter = require('./checkout.route.js')
 
 module.exports = (app) => {
     app.use(categoryMiddleware.category);
@@ -15,5 +16,8 @@ module.exports = (app) => {
     app.use("/products", productRouter);
 
     app.use("/search", searchRouter);
+
     app.use("/cart", cartRouter);
+
+    app.use("/checkout", checkoutRouter);
 }
