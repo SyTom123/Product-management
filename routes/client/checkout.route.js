76c8385de9d8b8b605203430d6045c3174660e9f
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-10-18 13:21:58 +0700

    Lấy data được gửi lên lưu vào database

--- routes/client/checkout.route.js
+++ routes/client/checkout.route.js
@@ -2,5 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../../controllers/client/checkout.controller');
 
-router.get("/", controller.index )
+router.get("/", controller.index );
+
+router.post("/order", controller.order)
 module.exports = router
