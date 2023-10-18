d24b17ecafa0dfc5d35b2d034213e0ef139ceea2
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-10-18 09:20:26 +0700

    Cập nhật số lượng sản phẩm trong giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -23,3 +23,24 @@ buttonGoBack.forEach(button => {
     })
 })
 // End Button go back
+// Cập nhật số lượng sản phẩm trong giỏ hàng
+const tableCart = document.querySelector('[table-cart]');
+
+if(tableCart) {
+    const inputsQuantity = document.querySelectorAll("input[name='quantity']");
+    if(inputsQuantity) {
+        inputsQuantity.forEach(input => {
+
+            input.addEventListener('change', ()=> {
+                const productId = input.getAttribute('product-id');
+                const quantity = input.value;
+
+                if(quantity > 0) {
+                    window.location.href =`cart/update/${productId}/${quantity}`
+                }
+            })
+        })
+    }
+}
+
+// Hết Cập nhật số lượng sản phẩm trong giỏ hàng
--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -8,5 +8,7 @@ router.post("/add/:productId", controller.addPost);
 
 router.get("/delete/:productId", controller.delete);
 
+router.get(`/update/:productId/:quantity`, controller.updateQuantity);
+
 module.exports = router;
 
