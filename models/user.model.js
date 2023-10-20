6857b1d782397aa149460a11a057beb259c65098
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-10-20 08:40:39 +0700

    Tạo collection users

---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const generate = require("../helper/generate");
+
+const userSchema = new mongoose.Schema({
+    fullName: String,
+    email: String,
+    password:String,
+    tokenUser: {
+        type: String,
+        default: generate.generateRandomString(30)
+    },
+    phone: String,
+    avatar: String,
+    status: {
+        type: String,
+        default: "active"
+    },
+    deleted: {
+        type: Boolean,
+        default: false
+    }, 
+    deleted: Date
+}, {timestamps: true});
+
+const User = mongoose.model("User", userSchema, "users");
+module.exports = User;
