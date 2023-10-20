081ab9bb1c33c719c2f9585dfbfd6f0dfcef7fa6
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-10-20 08:27:53 +0700

    1.4. Làm trang đặt hàng thành công

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -83,3 +83,28 @@ module.exports.order = async(req, res) => {
 
     res.redirect(`/checkout/success/${order.id}`);
 }
+// [GET] /checkout/success/:orderId
+module.exports.success = async (req, res) => {
+
+    const order = await Order.findOne({
+        _id: req.params.orderId
+    })
+
+    for(const product of order.products) {
+        const productInfo = await Product.findOne({
+            _id: product.product_id
+        }).select('title thumbnail')
+        console.log(productInfo);
+
+        product.productInfo = productInfo;
+
+        product.priceNew = productHelper.newPrice(product);
+        product.totalPrice = product.priceNew * product.quantity;
+
+    }
+    order.totalPrice = order.products.reduce((sum, item)=>sum + item.totalPrice, 0);
+    res.render("client/pages/checkout/success", {
+        pageTitle: "Đặt hàng thành công",
+        order: order
+    })
+}
