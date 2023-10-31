16d4ef6daa9bb0c797325a0a96ac0662e8422f2f
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-10-31 08:26:58 +0700

    Chèn icon

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,16 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
+
 // CLIEND_SEND_MESSAGE
 const formSendData = document.querySelector(".chat .inner-form");
-if(formSendData) {
-    formSendData.addEventListener("submit", (e)=> {
+if (formSendData) {
+    formSendData.addEventListener("submit", (e) => {
         e.preventDefault();
         const content = e.target.elements.content.value;
-        if(content) {
+        if (content) {
             socket.emit("CLIEND_SEND_MESSAGE", content);
             e.target.elements.content.value = "";
         }
-    })
+    });
 }
 // END CLIEND_SEND_MESSAGE
 
@@ -17,33 +19,55 @@ if(formSendData) {
 socket.on("SERVER_RETURN_MESSAGE", (data) => {
     const myId = document.querySelector("[my-id]").getAttribute("my-id");
     const body = document.querySelector(".chat .inner-body");
-  
+
     const div = document.createElement("div");
-  
+
     let htmlFullName = "";
-  
-    if(myId == data.userId) {
-      div.classList.add("inner-outgoing");
+
+    if (myId == data.userId) {
+        div.classList.add("inner-outgoing");
+    } else {
+        div.classList.add("inner-incoming");
+        htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
     }
-    else {
-      div.classList.add("inner-incoming");
-      htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
-    }
-  
+
     div.innerHTML = `
       ${htmlFullName}
       <div class="inner-content">${data.content}</div>
     `;
-  
+
     body.appendChild(div);
-    body.scrollTop = bodyChat.scrollHeight
-  });
-  // End SERVER_RETURN_MESSAGE
+    body.scrollTop = bodyChat.scrollHeight;
+});
+// End SERVER_RETURN_MESSAGE
 //SERVER_RETURN_MESSAGE
 
 // Scroll chat
 const bodyChat = document.querySelector(".chat .inner-body");
-if(bodyChat) {
-  bodyChat.scrollTop = bodyChat.scrollHeight
+if (bodyChat) {
+    bodyChat.scrollTop = bodyChat.scrollHeight;
 }
 // End Scroll chat
+
+// emoji-picker
+    // show popup
+const buttonIcon = document.querySelector(".button-icon");
+if(buttonIcon) {
+    const tooltip = document.querySelector(".tooltip");
+    Popper.createPopper(buttonIcon,tooltip);
+
+    buttonIcon.onclick = () => {
+        tooltip.classList.toggle('shown')
+    }
+}
+    //insert icon
+const emojiPicker = document.querySelector("emoji-picker");
+if (emojiPicker) {
+    const inputChat = document.querySelector(".chat .inner-form input[name='content']");
+
+    emojiPicker.addEventListener("emoji-click", (e) => {
+        const icon = e.detail.unicode;
+        inputChat.value = inputChat.value + icon;
+    });
+}
+//End emoji-picker
