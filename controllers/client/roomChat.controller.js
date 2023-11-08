bf4a54055cb411fabf200d09bcc428b513f78e33
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-11-08 11:03:32 +0700

    Tạo phòng chat

--- controllers/client/roomChat.controller.js
+++ controllers/client/roomChat.controller.js
@@ -1,6 +1,50 @@
+const User = require("../../models/user.model");
+const RoomChat = require("../../models/rooms-chat.model");
 //[GET] /rooms-chat
 module.exports.index = async(req, res) => {
     res.render("client/pages/rooms-chat/index", {
         pageTitle: "Danh sách phòng chat"
     })
+}
+
+//[GET] /rooms-chat/create
+module.exports.create = async(req, res) => {
+    const friendList = res.locals.user.friendList;
+
+    for(const friend of friendList) {
+        const infoFriend =  await User.findOne({
+            _id: friend.user_id
+        }).select("fullName avatar");
+
+        friend.infoFriend = infoFriend
+    }
+    res.render("client/pages/rooms-chat/create", {
+        pageTitle: "Tạo mới phòng chat",
+        friendList: friendList
+    })
+}
+//[POST] /rooms-chat/create
+module.exports.createPost = async(req, res) => {
+    const title = req.body.title;
+    const usersId = req.body.usersId;
+
+    const dataChat = {
+        title: title,
+        typeRoom : "group",
+        users: [],
+    }
+    usersId.forEach(userId => {
+        dataChat.users.push ({
+            user_id: userId,
+            role: "user"
+        })
+    })
+
+    dataChat.users.push ({
+        user_id: res.locals.user.id,
+        role: "superAdmin"
+    })
+    const room = new RoomChat(dataChat);
+    await room.save();
+    res.redirect(`/chat/${room.id}`);
 }
--- routes/client/roomChat.route.js
+++ routes/client/roomChat.route.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../../controllers/client/roomChat.controller');
-const chatMiddleware = require("../../middlewares/client/chat.middleware")
+const chatMiddleware = require("../../middlewares/client/chat.middleware");
+
 router.get("/", controller.index);
+
+router.get("/create", controller.create);
+
+router.post("/create", controller.createPost);
 
 module.exports = router;
